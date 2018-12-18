004b779008ca726f430460643d8a241d7d8de75a
unknown <britojas000@warren.k12.in.us>
2018-12-18 13:12:00 -0500

    Js

---
+++ jQuery/brains.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+    $(".clickMe").click(function() {
+
+        $('.hideMe').show();
+
+    });
+
+})
+
+
+
+
+/*
+EVENTS
+
+.click()
+.dblclick()
+.hover()
+.mouseenter()
+.mouseleave()
+
+
+
+FUNCTIONS
+
+.show()
+.hide()
+.toggle()
+.fadeIn()
+.fadeOut()
+.fadeToggle()
+.fadeTo()
+
+.on()
+
+.slideDown()
+.slideUp()
+.slideToggle()
+
+.animate()
+.css
+.addClass()
+.removeClass()
+.toggleClass()
+
+.text()
+.html()
+
+
+
+
+*/
